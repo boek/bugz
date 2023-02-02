332816c55e285eea433eeaa03bf8217fcf61936e
Jeff Boek <jeff@jeffboek.com>
2023-02-01 20:55:18 -0800

    lol tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,4 +5,12 @@ module.exports = {
     extend: {},
   },
   plugins: [],
+  safelist: [
+    "bg-rose-400",
+    "shadow-rose-400/50",
+    "bg-emerald-400",
+    "shadow-emerald-400/50",
+    "bg-sky-400",
+    "shadow-sky-400/50",
+  ]
 };
